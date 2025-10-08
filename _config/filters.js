217ef41a4544a28c57c3aec36eb8aea9ff1a8ba8
Ryan Robinson <ryan@ryanrobinson.ca>
2025-10-08 17:59:29 -0400

    Filter tagPages out of the list of tags

--- _config/filters.js
+++ _config/filters.js
@@ -34,7 +34,7 @@ export default function (eleventyConfig) {
   });
 
   eleventyConfig.addFilter("filterTagList", function filterTagList(tags) {
-    return (tags || []).filter(tag => ["all", "posts", "sidebar"].indexOf(tag) === -1).sort();
+    return (tags || []).filter(tag => ["all", "posts", "sidebar", "tagPages"].indexOf(tag) === -1).sort();
   });
 
   eleventyConfig.addFilter("postsWithMetadata", function (posts, metadataKey, metadataValue) {
