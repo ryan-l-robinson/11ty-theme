c636602080e0bb640184af190a47dda11af149f8
Ryan Robinson <ryrobinson@wlu.ca>
2025-12-19 14:38:32 -0500

    Added fallback text for when there is no description on a post

--- js/search.js
+++ js/search.js
@@ -57,29 +57,36 @@
 			const wrapper = document.createElement('div');
 			wrapper.classList.add('search-results-wrapper');
 
-      searchInput.setAttribute('aria-expanded', 'true');
+			// For accessibility, mark the region as expanded.
+			searchInput.setAttribute('aria-expanded', 'true');
+			// Start the list of results.
       const resultList = document.createElement('ul');
 
+			// Heading for clarity and better navigation.
       const sr_heading = document.createElement('h2');
       sr_heading.textContent = `${results.length} Search Result${results.length === 1 ? '' : 's'} for "${query}"`;
       sr_heading.classList.add('search-results-heading');
       wrapper.appendChild(sr_heading);
 
+			// Results added as list items.
       results.forEach(function (result) {
         const doc = idx.documentStore.docs[result.ref];
 				if (doc) {
 					// Create a list item for each result
-          const listItem = document.createElement('li');
+					const listItem = document.createElement('li');
           const link = document.createElement('a');
           link.href = doc.id; // The 'id' is the URL
           link.textContent = doc.title;
           listItem.appendChild(link);
 
+          const description = document.createElement('p');
           if (doc.description) {
-            const description = document.createElement('p');
             description.textContent = doc.description;
-            listItem.appendChild(description);
-          }
+					}
+					else {
+						description.textContent = "No description available."
+					}
+          listItem.appendChild(description);
 
 					resultList.appendChild(listItem);
         }
